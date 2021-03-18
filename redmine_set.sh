1c84397332337ebca3e29980dd00acd8d8c265e8
koharus0309 <saito_freude128@icloud.com>
2021-03-18 19:52:47 +0900

    redmine github hook pluginを追加
    redmine_set.shでインストールするpluginにredmine github hook pluginを追加した。
    本コミットでは、Dockerfileによるpluginの追加を断念し、pluginのsettingは全てredmine_set.shによって行うようにした。

--- redmine_set.sh
+++ redmine_set.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-docker exec -u root redmine_container sh -c "git clone https://github.com/farend/scm-creator.git /usr/src/redmine/plugins/redmine_scm"
-docker exec -u root redmine_container sh -c "chown redmine:redmine -R /usr/src/redmine/plugins/redmine_scm"
-docker exec -u redmine redmine_container bundle install --jobs=4 --without development test
-docker exec -u root  redmine_container bundle exec rake redmine:plugins:migrate RAILS_ENV=production
-docker exec redmine_container passenger-config restart-app /usr/src/redmine
+docker exec -u root my-pm-redmine sh -c "git clone https://github.com/farend/scm-creator.git /usr/src/redmine/plugins/redmine_scm"
+docker exec -u root my-pm-redmine sh -c "chown redmine:redmine -R /usr/src/redmine/plugins/redmine_scm"
+docker exec -u root docker_redmine-container_1 sh -c "git clone https://github.com/koppen/redmine_github_hook.git /usr/src/redmine/plugins/redmine_github_hook"
+docker exec -u root docker_redmine-container_1 sh -c "chown redmine:redmine -R /usr/src/redmine/plugins/redmine_github_hook"
+
+docker exec -u redmine my-pm-redmine bundle install --jobs=4 --without development test
+docker exec -u root my-pm-redmine bundle exec rake redmine:plugins:migrate RAILS_ENV=production
+docker exec my-pm-redmine passenger-config restart-app /usr/src/redmine
